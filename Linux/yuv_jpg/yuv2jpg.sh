5e6be384e40500998cc40e30309364b607c34167
celestin <arch.liu@foxmail.com>
2019-08-26 16:33:10 +0800

    Update yuv2jpg.sh
    规范文件和目录的变量定义

--- Linux/yuv_jpg/yuv2jpg.sh
+++ Linux/yuv_jpg/yuv2jpg.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 #Author:star
 #Time:2019-08-26
 #说   明：yuv2jpg.sh
 #1.转换YUV为JPG
 #根据call.rect将stage1:T&stage2:F放入stage1，
 #根据call.rect将stage1:T&stage2:T放入stage2
-#2.转换后的yuv文件放入子文件夹/stage1/yuv、/stage2/yuv
+#2.转换后的yuv文件放入子文件夹./stage1/yuv、./stage2/yuv
 #3.call.rect记录结构：对call.rect进行结构优化和stage1和stage2提取生成新的
 #call_stage1_verison.rect：记录stage1:T&stage2:F(先写入文件再转换)
 #call_stage2_version.rect：记录stage1:T&stage2:T(先写入文件再转换)
@@ -13,22 +14,37 @@
 #修改记录：(从高到底排序)
 #V0.1
 # ffmpeg cmd must add '< /dev/null'
-#!/bin/bash
 
-stage1_jpg=stage1
-stage2_jpg=stage2
-list_file=CALL.rect
 version="1.0.0.17"
-if [ ! -e "./$stage1_jpg"  ];then
-    \mkdir ./$stage1_jpg
-else
-    \rm ./$stage1_jpg/*
-fi
-if [ ! -e "./$stage2_jpg"  ];then
-    \mkdir ./$stage2_jpg
-else
-    \rm ./$stage2_jpg/*
-fi
+list_file=CALL.rect
+
+# file lists
+stage1_rect=call_stage1_${version}.rect
+stage2_rect=call_stage2_${version}.rect
+filename="$stage1_rect $stage2_rect"
+
+# storage directory
+stage1=stage1
+stage2=stege2
+stage1_yuv=./${stage1}/yuv
+stage2_yuv=./${stage2}/yuv
+dirname="$stage1 $stage2 $stage1_yuv $stage2_yuv"
+
+for i in $dirname
+do
+  if [ ! -e "./$i" ]; then
+      \mkdir -p ./$i
+  else
+      \rm -r ./$i/*
+  fi
+done
+
+for i in $filename
+do
+  if [ -e "./$i" ]; then
+      \rm ./$i
+  fi
+done
 
 line_num=$(\wc -l < $list_file)
 
@@ -47,16 +63,16 @@ do
         then
             if [[ $stage1 =~ 'stage1:T' && $stage2 =~ 'stage2:F' ]]; then
                 echo "stage1"
-                \ffmpeg -loglevel quiet -y -s 640x360 -i $yuv_file ./${stage1_jpg}/${yuv_file%.*}$version.jpg < /dev/null
-		cp $yuv_file ./stage1/yuv
-		#写入记录新文件call_stage1_version.rect
-		echo $line >> call_stage1_$version.rect
+                #写入记录新文件call_stage1_version.rect
+                echo $line >> $stage1_rect
+                \ffmpeg -loglevel quiet -y -s 640x360 -i $yuv_file ./${stage1}/${yuv_file%.*}$version.jpg < /dev/null
+                \cp $yuv_file ./$stage1_yuv
             elif [[ $stage1 =~ 'stage1:T' && $stage2 =~ 'stage2:T' ]]; then
                 echo "stage1 & stage2"
-                \ffmpeg -loglevel quiet -y -s 640x360 -i $yuv_file ./${stage2_jpg}/${yuv_file%.*}$version.jpg < /dev/null
-		cp $yuv_file ./stage2/yuv
-		#写入记录新文件call_stage1_version.rect
-		echo $line >> call_stage2_$version.rect
+                #写入记录新文件call_stage2_version.rect
+                echo $line >> $stage2_rect
+                \ffmpeg -loglevel quiet -y -s 640x360 -i $yuv_file ./${stage2}/${yuv_file%.*}$version.jpg < /dev/null
+                \cp $yuv_file ./$stage2_yuv
             else
                 echo ""
             fi
